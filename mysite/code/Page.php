87d5a007cff05a6f84914d5c093d60391980cb98
Dustin Quam <quamsta@gmail.com>
2017-12-01 09:06:25 -0600

    updates

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -2,7 +2,7 @@
 class Page extends SiteTree {
 
 	private static $db = array(
-		
+		'PageSpecificHTML' => 'HTMLText'
 	);
 
 	private static $has_one = array(
@@ -17,12 +17,45 @@ class Page extends SiteTree {
 
     private static $plural_name = "Pages";
 
-	private static $defaults = array ();
+	private static $defaults = array (
+		'PageSpecificHTML' => '<!-- Sojern Tag v6_js, Pixel Version: 1 -->
+			<script>
+			  (function () {
+			    /* Please fill the following values. */
+			    var params = {
+			       hd1: "", /* Check In Date. Format yyyy-mm-dd. Ex: 2015-02-14 */
+			       hd2: "", /* Check Out Date. Format yyyy-mm-dd. Ex: 2015-02-14 */
+			       hc1: "", /* Destination City */
+			       hs1: "", /* Destination State or Region */
+			       hpr: "", /* Hotel Property */
+			       hr: "", /* Number of Rooms */
+			       hpid: "", /* Property ID */
+			       t: "" /* Number of Travelers */
+			    };
+
+			    /* Please do not modify the below code. */
+			    var cid = [];
+			    var paramsArr = [];
+			    var cidParams = [];
+			    var pl = document.createElement(\'script\');
+			    var defaultParams = {"vid":"hot"};
+			    for(key in defaultParams) { params[key] = defaultParams[key]; };
+			    for(key in cidParams) { cid.push(params[cidParams[key]]); };
+			    params.cid = cid.join(\'|\');
+			    for(key in params) { paramsArr.push(key + \'=\' + encodeURIComponent(params[key])) };
+			    pl.type = \'text/javascript\';
+			    pl.async = true;
+			    pl.src = \'https://beacon.sojern.com/pixel/p/94111?f_v=v6_js&p_v=1&\' + paramsArr.join(\'&\');
+			    (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(pl);
+			  })();
+			</script>
+			<!-- End Sojern Tag -->'
+	);
 
 
 	public function getCMSFields(){
 		$f = parent::getCMSFields();
-		
+		$f->addFieldToTab('Root.Main', TextareaField::create('PageSpecificHTML', 'Page specific scripts (tracking codes)'), 'Content');
 		return $f;
 	}
 
--- themes/ihh/templates/Layout/RoomHolder.ss
+++ themes/ihh/templates/Layout/RoomHolder.ss
@@ -32,4 +32,4 @@
 	</div>
 </div>
 <% include TopicsAndNews %>
-
+$PageSpecificHTML
